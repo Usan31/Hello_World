e425c0ce153704a4021aa46f348ccb8465c8aa98
UsanShowtek <79810500+Usan31@users.noreply.github.com>
2021-03-19 20:35:53 -0600

    Create First Program-Python.py

---
+++ First Program-Python.py
@@ -0,0 +1,24 @@
+#Este es una practica sobre el codigo que eh estado aprendiendo en python
+
+""" Un comentario multilinea viendo 
+    como funciona los comentarios 
+    a ver si sale """ 
+
+#Empezamos con un saludo como siempre 
+print("+++++++++++++++++++++++++++++++")
+name1 = "Hola" #un comando simple como el dar datos a la variable
+text1 = """ mi nombre es Isaac, soy un programador junior
+que apenas estoy aprendiendo el lenguaje de python
+ojala me salga bien """
+print(name1 +  text1) #imprimimos el valor de las primeras variables 
+print()
+
+#otro texto, dando datos a las variables 
+text2 = "dos semanas"
+text3 = "Python"
+text4 = "Pseint"
+#ah imprimir, pero usando los llamados F-strings 
+print(f"""llevo {text2} programando en {text3},  
+aunque estuve practicando logica en {text4}""")
+print("+++++++++++++++++++++++++++++++")
+
